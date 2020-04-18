481bdc84348a1ffc356e91a280283dd0041ecc0d
Nicholas Phillips <nwp2@illinois.edu>
2020-04-18 12:01:22 -0500

    made async

--- build/imageIO.d.ts
+++
@@ -1,3 +0,0 @@
-import { bwImage, RGBImage } from './image';
-export declare function loadImage(path: string): Promise<unknown>;
-export declare function saveImageList(images: bwImage[] | RGBImage[], name?: string): void;
